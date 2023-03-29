fb7927096b672f23a03f78e3abbc50d922efb743
Eon4 <ladyforest85@gmail.com>
2023-03-29 10:21:16 +0200

    update

--- assets/js/site.js
+++ assets/js/site.js
@@ -25,17 +25,14 @@ navigator.geolocation.getCurrentPosition(success, error, geoOptions);
 
 function getLocationName(myLong, myLat) {
 
-
-
-    // geo code api https://locationiq.com
-    //const myApiKey = 'pk.439b01bdf321f632f87bd4d2631fe6d0';
+    /geo code api https://locationiq.com
+    const myApiKey = 'pk.439b01bdf321f632f87bd4d2631fe6d0';
     //const apiUrl = `https://eu1.locationiq.com/v1/reverse?key=${myApiKey}&lat=${myLat}&lon=${myLong}&format=json`;
-
-
 
 // geo code api https://nominatim  no token
 
 //url https://nominatim.openstreetmap.org/reverse?lat=56.94&lon=10.06&format=json
+
 const apiUrl = `https://nominatim.openstreetmap.org/reverse?lat=${myLat}&lon=${myLong}&format=json`;
 
 console.log(apiUrl);
